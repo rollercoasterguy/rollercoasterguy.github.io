e5fcab95ce54f00c908a89971ea326e88fbb0174
rollercoasterguy <rollercoastermeme@gmail.com>
2017-02-09 23:10:29 -0300

    dynamice slope roller-coaster

--- js/roller-coaster-engine.js
+++ js/roller-coaster-engine.js
@@ -1,94 +1,116 @@
 $(document).ready(function() {
-  $( "#donation_trigger" ).click();
+    $("#donation_trigger").click();
 
-  var tronaldDump = ["PANIC SELLING!!!", "DUMPPP!", "SWSF'S FAULT", "BIGGER BLOCKS CENTRALIZATION", "Cheina. China. Jina. Shyna", "buy da dip", "Bitcoin up to 10% off"];
-  var hodlersBelike = ["hooodl", "hodloor", "To the mooooon", "$10K INCOMING!!!", "BITCOIN WILL UNITE US!", "PUMP", "buckle up hodlers"];
-  var maximum = hodlersBelike.length;
-  var currentMoon = null;
-  var soMuchTxs = null;
+    var tronaldDump = ["PANIC SELLING!!!", "DUMPPP!", "SWSF'S FAULT", "BIGGER BLOCKS CENTRALIZATION", "Cheina. China. Jina. Shyna", "buy da dip", "Bitcoin up to 10% off"];
+    var hodlersBelike = ["hooodl", "hodloor", "To the mooooon", "$10K INCOMING!!!", "BITCOIN WILL UNITE US!", "PUMP", "buckle up hodlers"];
+    var meh = ["meh..", "mmm...", "meh", "mmm..", "no ban no fun", "meh", "no ban no fun"];
+    var maximum = hodlersBelike.length;
+    var currentMoon = null;
+    var soMuchTxs = null;
 
-  function moonTicker() {
-    $.ajax({
-      dataType: "json",
-      url: "https://api.bitfinex.com/v2/candles/trade:1h:tBTCUSD/last",
-      success: mooningFunction
-    });
-  }
-
-  function mooningFunction(data) {
-
-  	var oldEarth = data[1];
-  	currentMoon = data[2];
-
-  	var hodlerStatus = currentMoon>=oldEarth;
-
-    var randomDump = getRandom(maximum);
-    var randomPump = getRandom(maximum);
-
-    var rollerCoasterStatus = hodlerStatus ? hodlersBelike[randomPump] : tronaldDump[randomDump];
-    $('#current-moon').html('$'+currentMoon+" USD");
-
-    document.title = '('+Number(currentMoon).toFixed(1)+')' + " Bitcoin Roller Coaster Guy";
-
-    rBitcoin_or_rBtc(hodlerStatus);
-
-    $('#roller-coaster-status').html(rollerCoasterStatus);
-    feeRequest();
-  }
-
-  function rBitcoin_or_rBtc(hodlerStatus){
-    if(hodlerStatus){
-      $('#roller-coaster-guy').removeClass("here-we-go");
-      $('.panel').removeClass("panel-danger").addClass("panel-success");
-      $('.label').removeClass("label-danger").addClass("label-success");
-    }else {
-      $('#roller-coaster-guy').addClass("here-we-go");
-      $('.panel').removeClass("panel-success").addClass("panel-danger");
-      $('.label').removeClass("label-success").addClass("label-danger");
+    function moonTicker() {
+        $.ajax({
+            dataType: "json",
+            url: "https://api.bitfinex.com/v2/candles/trade:5m:tBTCUSD/hist?limit=6",
+            success: mooningFunction
+        });
     }
-  }
+
+    function mooningFunction(data) {
+        var oldEarth = data[5][1];
+        currentMoon = data[0][2];
+
+        var hodlerStatus = currentMoon >= oldEarth;
+
+        var angle = (Math.atan2(currentMoon - oldEarth, 15) * 180 / Math.PI);
+
+        $('#current-moon').html('$' + currentMoon + " USD");
+        document.title = '(' + Number(currentMoon).toFixed(1) + ')' + " Bitcoin Roller Coaster Guy";
+
+        updateStatus(hodlerStatus, angle);
+
+        feeRequest();
+    }
+
+    function updateStatus(hodlerStatus, angle) {
+        var absAngle = Math.abs(angle);
+        var randomNumber = getRandom(maximum);
+        var rollerCoasterStatus = "";
+        var angleTreshold = 15;
+
+        if (absAngle >= angleTreshold) {
+            $("#roller-coaster-guy").attr("src", "images/roller-coaster-guy.gif");
+            rotateTheGuy(90 - angle); //  +90 degrees 'cause de upwards gif
+
+            if (angle >= 0) {
+                rollerCoasterStatus = hodlersBelike[randomNumber];
+                $('.panel').removeClass("panel-danger").addClass("panel-success");
+                $('.label').removeClass("label-danger").addClass("label-success");
+            } else {
+                rollerCoasterStatus = tronaldDump[randomNumber];
+                $('.panel').removeClass("panel-success").addClass("panel-danger");
+                $('.label').removeClass("label-success").addClass("label-danger");
+            }
+        } else {
+            rollerCoasterStatus = meh[randomNumber];
+            $("#roller-coaster-guy").attr("src", "images/no-fun-roller-coaster-guy.gif");
+            rotateTheGuy(3 - angle);
+        }
+
+        $('#roller-coaster-status').html(rollerCoasterStatus);
+
+    }
+
+    function rotateTheGuy(angle) {
+        $("#roller-coaster-guy").css({
+            "transform": "rotate(" + angle + "deg)",
+            "-moz-transform": "rotate(" + angle + "deg)",
+            "o-transform": "rotate(" + angle + "deg)",
+            "webkit-transform": "rotate(" + angle + "deg)"
+        });
+    }
 
 
-  function getRandom(max) {
-    return Math.round(Math.random()*max);
-  }
+    function getRandom(max) {
+        return Math.round(Math.random() * max);
+    }
 
-  moonTicker();
-  setInterval(moonTicker, 10 * 1000);
-
-
-  //thread txCount request
-  function txCountRequest() {
-    $.ajax({
-      dataType: "json",
-      url: "https://blockchain.info/q/unconfirmedcount?cors=true",
-      success: mempoolAttack
-    });
-  }
-
-  function mempoolAttack(data){
-    soMuchTxs = data;
-    $('#tx-count').html(soMuchTxs);    
-  }
-
-  txCountRequest();
-  setInterval(txCountRequest, 30 * 1000);
+    moonTicker();
+    setInterval(moonTicker, 6 * 1000);
 
 
-  //thread for fee request
-  function feeRequest() {
-    $.ajax({
-      dataType: "json",
-      url: "https://bitcoinfees.21.co/api/v1/fees/recommended",
-      success: makeFeeGreatAgain
-    });
-  }
+    //thread txCount request
+    function txCountRequest() {
+        $.ajax({
+            dataType: "json",
+            url: "https://blockchain.info/q/unconfirmedcount?cors=true",
+            success: mempoolAttack
+        });
+    }
 
-  function makeFeeGreatAgain(data){
-      var fastestAvgFee = data.fastestFee;
-      var fastestAvgFeePerTx = ((fastestAvgFee * 226)/100000000) * currentMoon; 
-      $('#fastest-avg-fee').html("~$"+Number(fastestAvgFeePerTx).toFixed(3)+" USD");
-    
-  }
+    function mempoolAttack(data) {
+        soMuchTxs = data;
+        $('#tx-count').html(soMuchTxs);
+    }
+
+    txCountRequest();
+    setInterval(txCountRequest, 30 * 1000);
+
+
+    //thread for fee request
+    function feeRequest() {
+        $.ajax({
+            dataType: "json",
+            url: "https://bitcoinfees.21.co/api/v1/fees/recommended",
+            success: makeFeeGreatAgain
+        });
+    }
+
+    function makeFeeGreatAgain(data) {
+        var fastestAvgFee = data.fastestFee;
+        var fastestAvgFeePerTx = ((fastestAvgFee * 226) / 100000000) * currentMoon;
+        $('#fastest-avg-fee').html("~$" + Number(fastestAvgFeePerTx).toFixed(3) + " USD");
+
+    }
 
 });
