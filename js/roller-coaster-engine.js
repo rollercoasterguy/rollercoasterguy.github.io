f81aeb6c00e6524c4f96722a9831773b37f368e0
rollercoasterguy <rollercoastermeme@gmail.com>
2017-02-17 20:31:52 -0300

    minor changes

--- js/roller-coaster-engine.js
+++ js/roller-coaster-engine.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-    var tronaldDump = ["PANIC SELLING!!!", "DUMPPP!", "SWSF'S FAULT", "BIGGER BLOCKS CENTRALIZATION", "Cheina. China. Jina. Shyna", "buy da dip", "Bitcoin up to 10% off", "Tronald DUMP!"];
-    var hodlersBelike = ["hooodl", "hodloor", "To da mooooon", "$10K INCOMING!!!", "BITCOIN WILL UNITE US!", "PUMP", "buckle up hodlers", "can't see any altcoin up here"];
-    var meh = ["meh..", "mmm...", "meh", "mmm..", "no ban no fun", "meh", "no ban no fun", "mmm"];
+    var tronaldDump = ["PANIC SELLING!!!", "DUMPPP!", "SWSF'S FAULT", "BIGGER BLOCKS CENTRALIZATION", "Cheina. China. Jina. Shyna", "buy da dip", "Bitcoin up to 10% off", "Tronald DUMP!", "China ban. Ban china"];
+    var hodlersBelike = ["hooodl", "hodloor", "To da mooooon", "$10K INCOMING!!!", "BITCOIN WILL UNITE US!", "PUMP", "buckle up hodlers", "can't see any altcoin up here", "hey r/bitcoin, should I buy today?"];
+    var meh = ["meh..", "mmm...", "meh", "mmm..", "no ban no fun", "meh", "no ban no fun", "mmm", "no crisis, no pump"];
     var maximum = hodlersBelike.length;
     var currentMoon = null;
     var oldEarth = null;
@@ -23,8 +23,8 @@ $(document).ready(function() {
         var change = currentMoon - oldEarth;
         var angle = (Math.atan2(change, 15) * 180 / Math.PI);
 
-        $('#current-moon').html('$' + currentMoon + " USD");
         var signal = change >= 0 ? '+' : '-';
+        $('#current-moon').html('$' + currentMoon + " USD");
         $('#change-value').html(signal + Math.abs((change)).toFixed(2));
         $('#change-percentage').html(signal + Math.abs((((currentMoon / oldEarth) - 1) * 100)).toFixed(2) + "%");
         document.title = '(' + Number(currentMoon).toFixed(1) + ')' + " Bitcoin Roller Coaster Guy";
@@ -119,7 +119,6 @@ $(document).ready(function() {
         var fastestAvgFee = data.fastestFee;
         var fastestAvgFeePerTx = ((fastestAvgFee * 226) / 100000000) * currentMoon;
         $('#fastest-avg-fee').html("~$" + Number(fastestAvgFeePerTx).toFixed(3) + " USD");
-
     }
 
 });
